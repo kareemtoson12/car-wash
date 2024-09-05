4067087e758f9aa73db0c41fb8a654012f388e84
salmasafi <salmasafi26@gmail.com>
2024-09-05 16:49:13 +0300

    login4

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:clean_wash/features/main_screen.dart/view.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(DevicePreview(
     enabled: !kReleaseMode,
     builder: (context) => MyApp(), // Wrap your app
