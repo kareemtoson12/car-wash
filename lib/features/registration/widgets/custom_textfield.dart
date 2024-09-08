7f8c80a614e35ccde739fd4e7e1af7d30349ac6a
salmasafi <salmasafi26@gmail.com>
2024-09-08 16:56:44 +0300

    fix text field

--- lib/features/registration/widgets/custom_textfield.dart
+++ lib/features/registration/widgets/custom_textfield.dart
@@ -2,7 +2,7 @@ import 'package:clean_wash/core/colors_manger.dart';
 import 'package:clean_wash/core/styels_manger.dart';
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   const CustomTextField({
     super.key,
     required this.screenHeight,
@@ -19,28 +19,39 @@ class CustomTextField extends StatelessWidget {
   final bool obscureText;
 
   @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller,
+      controller: widget.controller,
       style: StylesManager.bodyText3,
       cursorColor: ColorsManger.grey,
       keyboardType: TextInputType.visiblePassword,
-      obscureText: obscureText,
+      obscureText: widget.obscureText,
       decoration: InputDecoration(
-        labelText: hintText,
+        labelText: widget.hintText,
         labelStyle: StylesManager.bodyText3,
-        suffixIcon: obscureText ? const Icon(Icons.remove_red_eye) : null,
+        suffixIcon: widget.obscureText
+            ? IconButton(
+                onPressed: () {
+                  if (widget.obscureText) {
+                    setState(() {
+                      widget.obscureText == false;
+                    });
+                  } else {
+                    setState(() {
+                      widget.obscureText == true;
+                    });
+                  }
+                },
+                icon: const Icon(Icons.remove_red_eye))
+            : null,
         border: OutlineInputBorder(
           borderSide: BorderSide(color: ColorsManger.grey),
-          borderRadius: BorderRadius.circular(screenWidth * 0.04),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: ColorsManger.grey),
-          borderRadius: BorderRadius.circular(screenWidth * 0.04),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: ColorsManger.grey),
-          borderRadius: BorderRadius.circular(screenWidth * 0.04),
+          borderRadius: BorderRadius.circular(widget.screenWidth * 0.04),
         ),
       ),
     );
